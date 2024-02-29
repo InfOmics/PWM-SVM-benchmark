211013343e7279dad8de7aabbcd394a8e3feeaf8
Manuel Tognon <51021763+ManuelTgn@users.noreply.github.com>
2024-02-29 12:45:33 +0100

    models trainig and evaluation

---
+++ src/models_training.py
@@ -0,0 +1,54 @@
+""" 
+"""
+
+import subprocess
+import sys
+import os
+
+MEMEDEFAULT = "-dna -mod zoops -nmotifs 1 -minw 6 -maxw 30 -revcomp"
+STREMEDEFAULT = "--objfun de --seed 42 --dna --nmotifs 1 --minw 8 --maxw 15"
+
+def gkmtrain(positive: str, negative: str, outdir: str) -> str:
+    modelprefix = os.path.join(outdir, os.path.splitext(os.path.basename(positive))[0])
+    try:
+        subprocess.run(["gkmtrain", "-T", "16", positive, negative, modelprefix])
+    except OSError as e:
+        raise OSError("SVM training failed")
+    return f"{modelprefix}.model.txt"
+
+def meme(positive: str, outdir: str) -> str:
+    outprefix = os.path.splitext(os.path.basename(positive))[0].replace("_train", "")
+    outdir = os.path.join(outdir, f"{outprefix}_meme")
+    try:
+        subprocess.call(f"meme -oc {outdir} {MEMEDEFAULT} {positive}", shell=True)
+    except OSError as e:
+        raise OSError("MEME training failed")
+    return os.path.join(outdir, "meme.txt")
+
+def streme(positive: str, negative: str, outdir: str) -> str:
+    outprefix = os.path.splitext(os.path.basename(positive))[0].replace("_train", "")
+    outdir = os.path.join(outdir, f"{outprefix}_streme")
+    try:
+        subprocess.call(f"streme -oc {outdir} {STREMEDEFAULT} --n {negative} --p {positive}", shell=True)
+    except OSError as e:
+        raise OSError("STREME training failed")
+    return os.path.join(outdir, "streme.txt")
+
+
+def main() -> None:
+    positive, negative, outdir = sys.argv[1:]
+    if not os.path.isdir(outdir):
+        os.mkdir(outdir)
+    # train SVM model
+    gkmtrain(positive, negative, outdir)
+    # train PWM model
+    meme(positive, outdir)
+    streme(positive, negative, outdir)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
