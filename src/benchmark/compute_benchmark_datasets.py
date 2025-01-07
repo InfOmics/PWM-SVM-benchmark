71e1d1d73d13d24c3128d4291f779f8a357c84b6
ManuelTgn <51021763+ManuelTgn@users.noreply.github.com>
2025-01-07 17:51:14 +0100

    improved models training

---
+++ src/benchmark/compute_benchmark_datasets.py
@@ -0,0 +1,169 @@
+""" 
+"""
+
+from typing import List, Tuple, Dict
+from glob import glob
+from tqdm import tqdm
+
+import pandas as pd
+
+import pybedtools
+import subprocess
+import random
+import sys
+import os
+
+
+# set seed for reproducibility
+random.seed(1234)
+
+# limit the analysis to consider only data mapped on canonical chromosomes
+CHROMS = [f"chr{c}" for c in list(range(1, 23)) + ["X", "Y"]]
+COMPARISONS = ["size", "width"]  # benchmark comparisons performed
+SIZES = [500, 1000, 2000, 5000, 10000, 0]  # dataset sizes compared
+
+
+def parse_commandline(args: List[str]) -> Tuple[str, str, str]:
+    """ """
+    if len(args) != 4:
+        raise ValueError(f"Too many/few input arguments ({len(args)})")
+    # parse input arguments from command line
+    comparison, traindatadir, genome, benchdir = args
+    if comparison not in COMPARISONS:  # benchmark comparison to perform
+        raise ValueError(f"Forbidden comparison requested ({comparison})")
+    if not os.path.isdir(traindatadir):  # check existence of train data folder
+        raise FileNotFoundError(f"Unable to locate train data folder ({traindatadir})")
+    # check genome reference file existence
+    if not os.path.isfile(genome):
+        raise FileNotFoundError(f"Unable to locate reference genome {genome}")
+    return comparison, traindatadir, genome, benchdir
+
+
+def read_bed(bedfile: str) -> pd.DataFrame:
+    """ """
+    return pd.read_csv(bedfile, header=None, sep="\t")  # load input bed file
+
+
+def sort_bed(bed: pd.DataFrame, prefix: str, size: int) -> pybedtools.BedTool:
+    """ """
+    bed = bed.sort_values([8, 6], ascending=False)  # sort peaks by q-value and enrichment 
+    size = bed.shape[0] if size == 0 else size  # 0 for full dataset
+    bedout = f"{prefix}_train.bed"
+    # to avoid training bias, shuffle the order of peaks
+    bed[:size].sample(frac=1).to_csv(bedout, sep="\t", header=False, index=False)
+    return pybedtools.BedTool(bedout)
+
+
+def extract_sequences(bed: pybedtools.BedTool, genome: str) -> str:
+    """ """
+    sequences = bed.sequence(fi=genome)  # extract sequences from reference
+    fasta = f"{os.path.splitext(bed.fn)[0]}.fa"
+    with open(fasta, mode="w") as outfile:
+        outfile.write(open(sequences.seqfn).read())
+    assert os.path.isfile(fasta)
+    return fasta
+
+
+def read_fasta(fasta: str) -> Dict[str, str]:
+    """ """
+    try:
+        with open(fasta, mode="r") as infile:  # recover seqnames
+            seqnames = [line.strip()[1:] for line in infile if line.startswith(">")]
+        with open(fasta, mode="r") as infile:  # recover sequences
+            sequences = [line.strip() for line in infile if not line.startswith(">")]
+    except OSError as e:
+        raise OSError(f"An error occurred while reading {fasta}") from e
+    return {seqname: sequences[i] for i, seqname in enumerate(seqnames)}
+
+
+def select_shuffle_bg_size(fastapos: str, fastaneg: str, prefix: str) -> None:
+    """ """
+    # retrieve sequence names for positive and negative sequences
+    sequencespos, sequencesneg = read_fasta(fastapos), read_fasta(fastaneg)
+    selected_sequences = [f"{seqname}_shuf" for seqname in sequencespos]
+    # write negative train sequences fasta
+    fastabg_size = f"{prefix}_shuffle_neg_train.fa"
+    try:
+        with open(fastabg_size, mode="w") as outfile:
+            for seqname in selected_sequences:
+                outfile.write(f">{seqname}\n{sequencesneg[seqname]}\n")
+    except OSError as e:
+        raise OSError("An error occurred while writing shuffle background sequences") from e
+    
+
+def chrom_split(fname: str) -> Dict[str, List[str]]:
+    """ """
+    chrom_dict = {c: [] for c in CHROMS}
+    with open(fname, mode="r") as infile:
+        for line in infile:  # group each feature in bed by chromosome
+            chrom = line.strip().split()[0]
+            chrom_dict[chrom].append(line)
+    return chrom_dict
+
+def compute_dnase_bg_size(trainpos: str, trainneg: str, prefix: str) -> Tuple[str, str]:
+    """ """
+    # split genomic features by chromosome
+    trainpos_chrom, trainneg_chrom = chrom_split(trainpos), chrom_split(trainneg)
+    bgtrain = []
+    for chrom in trainpos_chrom:  # select train features
+        bgtrain.extend(random.sample(trainneg_chrom[chrom], len(trainpos_chrom[chrom])))
+    trainneg_fname = f"{prefix}_dnase_neg_train.bed"
+    with open(trainneg_fname, mode="w") as outfile:
+        outfile.write("".join(bgtrain))
+    return trainneg_fname
+
+
+def compute_datasets_size(benchdir: str, traindatadir: str, genome: str, shuffle: bool):
+    """ """
+    # define comparison root directory
+    rootdir = os.path.join(benchdir, "dataset-size-comparison")
+    trainposdir_fasta = os.path.join(rootdir, "shuffle/train/fasta/positive") if shuffle else os.path.join(rootdir, "dnase/train/fasta/positive")
+    trainposdir_bed = os.path.join(rootdir, "shuffle/train/bed/positive") if shuffle else os.path.join(rootdir, "dnase/train/bed/positive")
+    trainnegdir_fasta = os.path.join(rootdir, "shuffle/train/fasta/negative") if shuffle else os.path.join(rootdir, "dnase/train/fasta/negative")
+    trainnegdir_bed = os.path.join(rootdir, "shuffle/train/bed/negative") if shuffle else os.path.join(rootdir, "dnase/train/bed/negative")
+    traindatadir = os.path.join(traindatadir, "shuffle") if shuffle else os.path.join(traindatadir, "dnase")
+    # retrieve positive bed files
+    posbeds = glob(os.path.join(traindatadir, "positive/*.bed"))
+    for size in SIZES:  # iterate over dataset sizes
+        sizedir = "size_full" if size == 0 else f"size_{size}"
+        sys.stdout.write(f"size - {sizedir}\n")
+        trainposdir_fasta_size = os.path.join(trainposdir_fasta, sizedir)
+        trainposdir_bed_size = os.path.join(trainposdir_bed, sizedir)
+        trainnegdir_fasta_size = os.path.join(trainnegdir_fasta, sizedir)
+        trainnegdir_bed_size = os.path.join(trainnegdir_bed, sizedir)
+        for d in [trainposdir_fasta_size, trainposdir_bed_size, trainnegdir_fasta_size, trainnegdir_bed_size]:
+            if not os.path.isdir(d):  # create outout folders if not already present
+                os.makedirs(d)
+        for posbed in tqdm(posbeds):  # iterate over positive bed files
+            bed_prefix = os.path.basename(posbed).split("_")[0]
+            bed_pos = sort_bed(read_bed(posbed), os.path.join(trainposdir_bed_size, bed_prefix), size)
+            fastapos = extract_sequences(bed_pos, genome)
+            if shuffle:  # compute synthetic background data
+                fastaneg = os.path.join(traindatadir, "negative", f"{bed_prefix}_shuffle_neg_train.fa")
+                # select background sequences based on the picked positive sequences
+                select_shuffle_bg_size(fastapos, fastaneg, os.path.join(trainnegdir_fasta_size, bed_prefix))
+            else:  # compute real biological background data
+                negbed = os.path.join(traindatadir, "negative", f"{bed_prefix}_dnase_neg_train.bed")
+                bed_neg = compute_dnase_bg_size(bed_pos.fn, negbed, os.path.join(trainnegdir_bed_size, bed_prefix))
+                fastaneg = extract_sequences(pybedtools.BedTool(bed_neg), genome)
+                subprocess.call(f"mv {fastaneg} {trainnegdir_fasta_size}", shell=True)
+            subprocess.call(f"mv {fastapos} {trainposdir_fasta_size}", shell=True)
+
+
+def main():
+    # parse input arguments (comparison to perform and benchmark data base folder)
+    # the script assumes that data directory follows the structure defined in the 
+    # previous step of the pipeline
+    comparison, traindatadir, genome, benchdir = parse_commandline(sys.argv[1:])
+    if not benchdir:
+        os.mkdir(benchdir)  # if not aslready present, create benchmark data folder
+    if comparison == COMPARISONS[0]:  # benchmark performance on dataset size
+        compute_datasets_size(benchdir, traindatadir, genome, True)  # synthetic bg
+        compute_datasets_size(benchdir, traindatadir, genome, False)  # real bg
+
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ src/benchmark/models_training.py
@@ -0,0 +1,121 @@
+"""
+"""
+
+from typing import List, Tuple, Set
+from glob import glob
+from tqdm import tqdm
+
+import subprocess
+import sys
+import os
+
+# computational tools used to train motif models
+TOOLS = ["lsgkm", "meme", "streme"]
+# default parameters for meme and streme (pwm models)
+MEMEDEFAULT = "-dna -mod zoops -nmotifs 1 -minw 6 -maxw 30 -revcomp"
+STREMEDEFAULT = "--objfun de --seed 42 --dna --nmotifs 1 --minw 8 --maxw 15"
+COMPARISONS = ["size", "width", "kernel"]  # benchmark comparisons performed
+SIZES = [500, 1000, 2000, 5000, 10000, 0]  # dataset sizes compared
+
+
+
+def parse_commandline(args: List[str]) -> Tuple[str, str, str]:
+    """ """
+    comparison, benchdatadir, benchmarkdir = args
+    if comparison not in COMPARISONS:  # check correct comparison
+        raise ValueError(f"Forbidden comparison requested ({comparison})")
+    if not os.path.isdir(benchdatadir):  # check benchmark data directory existence
+        raise FileNotFoundError(f"Unable to locate benchmark data folder ({benchdatadir})")
+    return comparison, benchdatadir, benchmarkdir
+
+
+def retrieve_experiment_names(datadir: str) -> Set[str]:
+    """ """
+    # retrieve positive and negative sequences fasta
+    positives = glob(os.path.join(datadir, "*.fa"))
+    return {os.path.basename(f).split("_")[0] for f in positives}
+
+
+def meme(sequences: str, outprefix: str) -> str:
+    """ """
+    outdir = f"{outprefix}_meme"  # output directory for meme model
+    try:
+        code = subprocess.call(f"meme -oc {outdir} {MEMEDEFAULT} {sequences}", shell=True, stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL,)
+        if code != 0:
+            raise subprocess.SubprocessError(f"MEME PWM training failed on {sequences}")
+    except OSError as e:
+        raise OSError("MEME training failed") from e
+    
+
+def streme(positive: str, negative: str, outprefix: str) -> str:
+    """ """
+    outdir = f"{outprefix}_streme"  # output directory for streme model
+    try:
+        code = subprocess.call(f"streme -oc {outdir} {STREMEDEFAULT} --n {negative} --p {positive}", shell=True, stderr=subprocess.STDOUT, stdout=subprocess.DEVNULL)
+        if code != 0:
+            raise subprocess.SubprocessError(f"STREME PWM training failed on {positive}")
+    except OSError as e:
+        raise OSError("STREME training failed") from e
+    
+def gkmtrain(positive: str, negative: str, kernel: int, outprefix: str) -> str:
+    """ """
+    # compute svm model using lsgkm
+    outdir = f"{outprefix}_svm"  # output directory for lsgkm
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
+    modelprefix = os.path.join(outdir, os.path.basename(outprefix))
+    # RBF kernels (3 and 5) work best with -c 10 -g 2
+    options = "-c 10 -g 2" if kernel in [3, 5] else ""
+    try:  # run lsgkm
+        code = subprocess.call(
+            f"gkmtrain -t {kernel} {options} -T 16 {positive} {negative} {modelprefix}", 
+            shell=True,
+            stderr=subprocess.STDOUT,
+            stdout=subprocess.DEVNULL,
+        )
+        if code != 0:
+            raise subprocess.SubprocessError(f"SVM training failed on {positive}")
+    except OSError as e:
+        raise OSError("SVM training failed") from e
+
+
+def train_models_size(benchdatadir: str, bg: str, modelsdir: str):
+    """ """
+    # retrieve experiment names
+    experiment_names = retrieve_experiment_names(os.path.join(benchdatadir, f"{bg}/train/fasta/positive/size_500"))
+    for size in SIZES:  # iterate over dataset sizes
+        sizedir = "size_full" if size == 0 else f"size_{size}"
+        sys.stdout.write(f"size - {sizedir}\n")
+        trainposdir = os.path.join(benchdatadir, f"{bg}/train/fasta/positive/", sizedir)
+        trainnegdir = os.path.join(benchdatadir, f"{bg}/train/fasta/negative/", sizedir)
+        modelsoutdir = os.path.join(modelsdir, bg, sizedir)
+        if not os.path.isdir(modelsoutdir):
+            os.makedirs(modelsoutdir)
+        for experiment_name in tqdm(experiment_names):
+            positive = os.path.join(trainposdir, f"{experiment_name}_train.fa")
+            negative = os.path.join(trainnegdir, f"{experiment_name}_{bg}_neg_train.fa")
+            meme(positive, os.path.join(modelsoutdir, experiment_name))  # meme train
+            # streme(positive, negative, os.path.join(modelsoutdir, experiment_name))  # streme train
+            # gkmtrain(positive, negative, 4, os.path.join(modelsoutdir, experiment_name))  # lsgkm train
+
+
+def train_models(comparison: str, benchdatadir: str, benchmarkdir: str):
+    """ """
+    if comparison == COMPARISONS[0]:  # compare performanec on different dataset sizes
+        benchdatadir = os.path.join(benchdatadir, "dataset-size-comparison")
+        modelsdir = os.path.join(benchmarkdir, "models/dataset-size-comparison")
+        train_models_size(benchdatadir, "shuffle", modelsdir)  # sythentic bg
+        train_models_size(benchdatadir, "dnase", modelsdir)  # real bg
+
+
+def main():
+    # parse input arguments from command line 
+    comparison, benchdatadir, benchmarkdir = parse_commandline(sys.argv[1:])
+    # train motif models
+    train_models(comparison, benchdatadir, benchmarkdir)
+
+
+if __name__ == "__main__":
+    main()
+
+
